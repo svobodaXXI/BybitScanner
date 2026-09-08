e6ef0e0e6ae66ca5894ebf7621016a0c501be4d1
svobodaXXI <99403019+svobodaXXI@users.noreply.github.com>
2026-09-08 12:10:26 +0300

    feat: export Trading Diary D2 contracts

--- terminal/diary/__init__.py
+++ terminal/diary/__init__.py
@@ -1,5 +1,22 @@
-"""Trading Diary observational read-model package."""
+"""Trading Diary observational/read-model package."""
 
+from .decision_models import (
+    DecisionEventId,
+    DecisionEventKind,
+    DecisionEventRecord,
+    OrderPlanId,
+    OrderPlanRecord,
+    SetupInstanceId,
+    SetupInstanceRecord,
+    SetupTerminalOutcome,
+    TradeEpisodeSetupLink,
+)
+from .decision_store import (
+    DiaryDecisionStore,
+    DiaryImmutableConflict,
+    DiaryLinkageError,
+    DiaryPersistenceError,
+)
 from .models import (
     AllocationRole,
     DiaryEnvironment,
@@ -13,11 +30,24 @@ from .service import TradeEpisodeReadService
 
 __all__ = [
     "AllocationRole",
+    "DecisionEventId",
+    "DecisionEventKind",
+    "DecisionEventRecord",
+    "DiaryDecisionStore",
     "DiaryEnvironment",
+    "DiaryImmutableConflict",
+    "DiaryLinkageError",
+    "DiaryPersistenceError",
     "ExecutionAllocation",
     "ExecutionFactView",
+    "OrderPlanId",
+    "OrderPlanRecord",
+    "SetupInstanceId",
+    "SetupInstanceRecord",
+    "SetupTerminalOutcome",
     "TradeEpisode",
     "TradeEpisodeId",
     "TradeEpisodeReadService",
     "TradeEpisodeReconstructor",
+    "TradeEpisodeSetupLink",
 ]
